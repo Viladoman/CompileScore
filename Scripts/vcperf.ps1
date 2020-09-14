7600918a836bfc87165911094c1a655073e4c109
ikrima <contact@ikrima.com>
2020-09-14 16:06:58 -0700

    Fix #3: Quality of life: powershell script for automation
    remove references to tolva and cppbuildanalyzer

--- Scripts/vcperf.ps1
+++ Scripts/vcperf.ps1
@@ -19,18 +19,16 @@ param(
   [switch]$start,
   #stop vcperf trace capture
   [switch]$stop,
-  #Runs CppBuildAnalyzer on vcperf trace
-  [switch]$cppbldanalyze,
   #Runs CompileScorer on vcperf trace
   [switch]$compilescore,
   #Output directory for analysis files
   [ValidateScript({if ($_){  Test-Path $_}})]
-  [string]$outdir = "$PSScriptRoot\..\..\.bin-int\vcperf",
+  [string]$outdir = "$PSScriptRoot",
   #Tools directory containing vcperf.exe, ScoreDataExtractor.exe, CppBuildAnalyzer.exe
   [ValidateScript({if ($_){  Test-Path $_}})]
-  [string]$tooldir = "$PSScriptRoot\..\..\tools\vcperf",
+  [string]$tooldir = "$PSScriptRoot",
   #Output directory for analysis files
-  [string]$seshname = "tolva"
+  [string]$seshname = "compilescore"
 )
 
 $ErrorActionPreference = "STOP"
@@ -39,34 +37,24 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 
 $vcperfExe       = "$tooldir\vcperf.exe"
 $compilescoreExe = "$tooldir\ScoreDataExtractor.exe"
-$cppbldanalExe   = "$tooldir\CppBuildAnalyzer.exe"
 $etltraceFile    = "$outdir\vcperf_$seshname.etl"
 $cmplScoreFile   = "$outdir\compileData.scor"
 
 if ($start) {
   Write-host "Starting vcperf trace capture" -ForegroundColor DarkGreen
-  gsudo cache on
-  gsudo $vcperfExe /start $seshname
-  # Start-Process powershell @('-command', '&', $vcperfExe, '/start', $seshname, '*>', "$outdir\vcperfscriptlog.txt") -Wait -Verb RunAs
-  # Get-Content "$outdir\vcperfscriptlog.txt"
+  Start-Process powershell @('-command', '&', $vcperfExe, '/start', $seshname, '*>', "$outdir\vcperfscriptlog.txt") -Wait -Verb RunAs
+  Get-Content "$outdir\vcperfscriptlog.txt"
 }
 elseif ($stop) {
   Write-host "Stopping vcperf trace capture" -ForegroundColor DarkGreen
-  gsudo $vcperfExe /stopnoanalyze $seshname $etltraceFile
-  # Start-Process powershell @('-command', '&', $vcperfExe, '/stopnoanalyze', $seshname, $etltraceFile, '*>', "$outdir\vcperfscriptlog.txt") -Wait -Verb RunAs
-  # Get-Content "$outdir\vcperfscriptlog.txt"
+  Start-Process powershell @('-command', '&', $vcperfExe, '/stopnoanalyze', $seshname, $etltraceFile, '*>', "$outdir\vcperfscriptlog.txt") -Wait -Verb RunAs
+  Get-Content "$outdir\vcperfscriptlog.txt"
 }
 
 
 Switch ($PSBoundParameters.GetEnumerator().
   Where({$_.Value -eq $true}).Key)
 {
-  'cppbldanalyze' {
-    Write-host "Running CppBuildAnalyzer" -ForegroundColor DarkGreen
-    Push-Location -Path $outdir
-    & $cppbldanalExe @('-i', $etltraceFile, '--analyze_all')
-    Pop-Location
-  }
   'compilescore' {
     Write-host "Running CompileScorer" -ForegroundColor DarkGreen
     Push-Location -Path $outdir
