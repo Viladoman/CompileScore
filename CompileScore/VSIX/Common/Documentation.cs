f267222325d17f476d8bd3016eef249305a46ec1
Ramon Viladomat <ramon.viladomat@gmail.com>
2020-12-26 17:09:03 -0500

    Fixed documentation links preparing for proper wiki pages

--- CompileScore/VSIX/Common/Documentation.cs
+++ CompileScore/VSIX/Common/Documentation.cs
@@ -20,7 +20,7 @@ namespace CompileScore
             {
                 case Link.MainPage:             return @"https://github.com/Viladoman/CompileScore";
                 case Link.ReportIssue:          return @"https://github.com/Viladoman/CompileScore/issues";
-                case Link.GeneralConfiguration: return @"https://github.com/Viladoman/CompileScore"; //TODO ~ ramonv ~ add this wiki page
+                case Link.GeneralConfiguration: return @"https://github.com/Viladoman/CompileScore/wiki/Configurations";
                 case Link.Donate:               return @"https://www.paypal.com/donate?hosted_button_id=QWTUS8PNK5X5A";
             }
             return null;
