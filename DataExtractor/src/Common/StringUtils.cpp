6e2a2dd28cba5caf654539c61499a6367a610bd5
Ramon Viladomat <ramon.viladomat@gmail.com>
2023-04-06 14:59:07 -0700

    Improved extractor path normalizer to remove ../ and ./ from full paths

--- DataExtractor/src/Common/StringUtils.cpp
+++ DataExtractor/src/Common/StringUtils.cpp
@@ -1,5 +1,7 @@
 #include "StringUtils.h"
 
+#include "BasicTypes.h"
+
 namespace StringUtils
 { 
     // -----------------------------------------------------------------------------------------------------------
@@ -54,6 +56,17 @@ namespace StringUtils
 	// -----------------------------------------------------------------------------------------------------------
 	void NormalizePath(fastl::string& input)
 	{
+        enum 
+        { 
+            FOLDER_BUFFER_SIZE = 16, 
+            FOLDER_BUFFER_MASK = FOLDER_BUFFER_SIZE - 1
+        };
+        static_assert(( FOLDER_BUFFER_SIZE & (FOLDER_BUFFER_SIZE - 1) ) == 0u, "FOLDER_BUFFER_SIZE must be a power of 2");        
+
+        size_t folderBuffer[FOLDER_BUFFER_SIZE];
+        U8 head = 0; 
+        U8 tail = 0;
+
         size_t writeIndex = 0; 
         bool wasForwardSlash = false;
         const size_t sz = input.length();
@@ -67,9 +80,46 @@ namespace StringUtils
 
             //remove consecutive forward slashes
             const bool isForwardSlash = input[i] == '/';
-            if (wasForwardSlash && isForwardSlash)
+            if (wasForwardSlash)
             {
-                continue;
+                if (isForwardSlash)
+                {
+                    continue;
+                }
+
+                //Handle directory changes
+				if (input[i] == '.')
+				{
+                    //Check for . ./ .\ or end of string to omit this character 
+                    size_t nextIndex = i + 1;
+                    if (nextIndex >= sz || input[nextIndex] == '/' || input[nextIndex] == '\\')
+                    {
+                        continue;
+                    }
+
+					//Check for .. ../ ..\ and move the writecursor back to the previous folder
+                    size_t nextNextIndex = nextIndex + 1;
+					if (input[nextIndex] == '.' && (nextNextIndex >= sz || input[nextNextIndex] == '/' || input[nextNextIndex] == '\\'))
+					{
+                        //retrieve the previous folder point if we have it
+                        head = (head - 1) & FOLDER_BUFFER_MASK;
+                        if (head != tail)
+                        {
+                            writeIndex = folderBuffer[head] + 1;
+                            ++i; //skip the second point on the next iteration
+                            continue;
+                        }
+					}
+
+				}
+            }
+
+            //Store folder split point
+            if (isForwardSlash)
+            {
+                head = (head+1) & FOLDER_BUFFER_MASK; 
+                tail = head == tail ? (head + 1) & FOLDER_BUFFER_MASK : tail;
+                folderBuffer[head] = writeIndex;
             }
 
             //lower case the full path 
