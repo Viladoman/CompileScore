266c28455a82c56b979949ff1883d0f9350ec0ab
Ramon Viladomat <ramon.viladomat@gmail.com>
2023-04-07 01:06:46 -0400

    Added .scor drop support to VS text editor and rearranged the common project folders

---
+++ CompileScore/Shared/Editor/Drop/DropHandler.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Windows;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Text.Editor.DragDrop;
+
+namespace CompileScore
+{
+    public class DropHandler : IDropHandler
+    {
+        //TODO ~ ramonv add support for .ctl and .etl for here and open file. Also add the option to generate the trace on generation
+        private static readonly List<string> supportedExtensions = new List<string> { ".scor" }; 
+
+        public DragDropPointerEffects HandleDragStarted(DragDropInfo dragDropInfo)
+        {
+            return DragDropPointerEffects.Link;
+        }
+
+        public DragDropPointerEffects HandleDraggingOver(DragDropInfo dragDropInfo)
+        {
+            return DragDropPointerEffects.Link;
+        }
+
+        public DragDropPointerEffects HandleDataDropped(DragDropInfo dragDropInfo)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            string filename = GetScoreFilename(dragDropInfo);
+            if (filename != null)
+            {
+                CompilerData.Instance.ForceLoadFromFilename(filename);
+                EditorUtils.FocusOverviewWindow();
+            }
+            return DragDropPointerEffects.Link;
+        }
+     
+        public bool IsDropEnabled(DragDropInfo dragDropInfo)
+        {
+            return GetScoreFilename(dragDropInfo) != null;
+        }
+
+        private static string GetScoreFilename(DragDropInfo Info)
+        {
+            DataObject Data = new DataObject(Info.Data);
+            if (Info.Data.GetDataPresent(DropHandlerProvider.FileDropDataFormat))
+            {
+                StringCollection Files = Data.GetFileDropList();
+                if (Files != null && Files.Count == 1 && !string.IsNullOrEmpty(Files[0]) && supportedExtensions.Contains(Path.GetExtension(Files[0])))
+                {
+                    return Files[0];
+                }
+            }
+
+            return null;
+        }
+
+        public void HandleDragCanceled()
+        {
+        }
+    }
+}
---
+++ CompileScore/Shared/Editor/Drop/DropHandlerProvider.cs
@@ -0,0 +1,22 @@
+﻿
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.VisualStudio.Text.Editor.DragDrop;
+
+namespace CompileScore
+{
+    [Export(typeof(IDropHandlerProvider))]
+    [DropFormat(DropHandlerProvider.FileDropDataFormat)]
+    [Name("Score Loader Drop Handler")]
+    [Order(Before = "DefaultFileDropHandler")]
+    internal class DropHandlerProvider : IDropHandlerProvider
+    {
+        internal const string FileDropDataFormat = "FileDrop";
+
+        public IDropHandler GetAssociatedDropHandler(IWpfTextView TextView)
+        {
+            return TextView.Properties.GetOrCreateSingletonProperty<DropHandler>(() => new DropHandler());
+        }
+    }
+}
