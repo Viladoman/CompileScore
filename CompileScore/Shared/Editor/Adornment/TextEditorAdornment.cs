3cc04af6b9aeb2738de1ee28c7fec3c0d245c9c0
Ramon Viladomat <ramon.viladomat@gmail.com>
2023-04-07 02:14:53 -0400

    Added skeleton for adornment to provide impact and severity data

---
+++ CompileScore/Shared/Editor/Adornment/TextEditorAdornment.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace CompileScore
+{
+    internal sealed class TextEditorAdornment
+    {
+        private const float AdornmentSize = 18;
+
+        private readonly IAdornmentLayer adornmentLayer;
+        private readonly IWpfTextView view;
+
+        private readonly UIElement adorment;
+        private readonly string fullPath;
+
+
+        public TextEditorAdornment(IWpfTextView view)
+        {
+            if (view == null)
+            {
+                throw new ArgumentNullException("view");
+            }
+
+            this.view = view;
+
+            fullPath = null;
+            ITextDocument document = null;
+            if (view.TextDataModel.DocumentBuffer.Properties.TryGetProperty(typeof(ITextDocument), out document))
+            {
+                fullPath = document.FilePath;
+            }
+
+            //TODO ~ ramonv ~ build something that can match the best similar path ( find all instances with the same name and then tries to match as many folders as possible )
+            //TODO ~ blend this together with the show timeline code
+
+            //TODO ~ create the proper UIElement
+
+            //TODO ~ create an array of static icons for each severity
+            adorment = new Image
+            {
+                // Get the image path from within the packaged extension
+                Source = new BitmapImage(
+                    new Uri(
+                        Path.Combine(
+                            Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+                            "Resources",
+                            "IconDetail.png"))),
+                Width = AdornmentSize,
+                Height = AdornmentSize,
+            };
+
+            
+
+            adornmentLayer = view.GetAdornmentLayer("TextEditorAdornment");
+
+            view.LayoutChanged += OnSizeChanged;
+        }
+
+        private void OnSizeChanged(object sender, EventArgs e)
+        {
+            adornmentLayer.RemoveAllAdornments();
+
+            Canvas.SetLeft(adorment, view.ViewportRight - AdornmentSize);
+            Canvas.SetTop(adorment, view.ViewportBottom - AdornmentSize);
+            adornmentLayer.AddAdornment(AdornmentPositioningBehavior.ViewportRelative, null, null, adorment, null);
+        }
+    }
+}
---
+++ CompileScore/Shared/Editor/Adornment/TextEditorAdornmentProvider.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+
+namespace CompileScore
+{
+    [Export(typeof(IWpfTextViewCreationListener))]
+    [ContentType("C/C++")]
+    [TextViewRole(PredefinedTextViewRoles.Document)]
+    internal sealed class TextEditorAdornmentTextViewCreationListener : IWpfTextViewCreationListener
+    {
+        // Disable "Field is never assigned to..." and "Field is never used" compiler's warnings. Justification: the field is used by MEF.
+#pragma warning disable 649, 169
+        [Export(typeof(AdornmentLayerDefinition))]
+        [Name("TextEditorAdornment")]
+        [Order(After = PredefinedAdornmentLayers.Caret)]
+        private AdornmentLayerDefinition editorAdornmentLayer;
+#pragma warning restore 649, 169
+
+        public void TextViewCreated(IWpfTextView textView)
+        {
+            //TODO ~ ramonv ~ disable until it is ready 
+            // The adorment will get wired to the text view events
+            //new TextEditorAdornment(textView);
+        }
+    }
+}
